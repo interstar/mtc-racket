f49776e015dbe74aaf20986b43540448c70862ee
phil jones <interstar@gmail.com>
2016-03-11 15:18:59 -0300

    now using racket 6, and installed sxml so using this to analyze grabbed web-pages to extract title

--- actions.rkt
+++ actions.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (require "tools.rkt" (planet neil/html-parsing:2:0)
-         net/url xml/path xml)
+         net/url sxml)
 
 (provide has-url extract-url grab-url url->bytes url->xexp title-from-url analyze
          page-path page-exists?)
@@ -19,23 +19,14 @@
 (define (url->xexp url) (html->xexp (grab-url url)))
 
 
-;; temporarily doing my own search here until I can get pkg installation working properly and install sxml package
-(define (search-sxml key doc)
-  (cond
-    [(empty? doc) ""]
-    [(list? doc) 
-     (if (eq? (car doc) key) (cdr doc)
-         (filter (lambda (x) (not (or (equal? x "") (equal? x '())))) (map (curry search-sxml key) doc)))]
-    [else ""]))
 
 (define (title-from-url url) 
   (let* ([xexp (url->xexp url)])
-    (search-sxml 'title xexp  )))
+    ((sxpath '(// title)) xexp  )))
 
 (define (analyze item) 
   (let* ([url (extract-url item)])
-    (~a (car (flatten (title-from-url url))))
-    ))
+    (~a  (title-from-url url))) )
 
 ;;; Files
 
